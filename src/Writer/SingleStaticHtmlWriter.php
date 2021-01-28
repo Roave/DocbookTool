711b2aca5c13534d877f1e52bec2de199c663e2a
James Titcumb <james@asgrim.com>
2021-01-28 10:57:46 +0000

    Use monolog for CLI logging

--- src/Writer/SingleStaticHtmlWriter.php
+++ src/Writer/SingleStaticHtmlWriter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Roave\DocbookTool\Writer;
 
+use Psr\Log\LoggerInterface;
 use Roave\DocbookTool\DocbookPage;
 use Twig\Environment;
 
@@ -14,13 +15,15 @@ final class SingleStaticHtmlWriter implements OutputWriter
     public function __construct(
         private Environment $twig,
         private string $twigTemplate,
-        private string $outputFile
+        private string $outputFile,
+        private LoggerInterface $logger
     ) {
     }
 
     /** @param DocbookPage[] $docbookPages */
     public function __invoke(array $docbookPages): void
     {
+        $this->logger->info('Writing HTML output to ' . $this->outputFile);
         file_put_contents(
             $this->outputFile,
             $this->twig->render($this->twigTemplate, ['pages' => $docbookPages])
